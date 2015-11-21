9677a69336079feb30ca5e11620d3fd97de99e13
John Leidel <john.leidel@ttu.edu>
2015-11-21 09:34:09 -0600

    adding initial cmc library template directory; template and build infrastructure

--- Makefile
+++ Makefile
@@ -4,31 +4,32 @@
 #
 
 include Makefile.inc
- 
+
 PKGS := ...
 LIBNAME := hmcsim
 SRCDIR := src
 BUILDDIR := build
 LIBS :=
 TARGET := lib$(LIBNAME).a
-LDFLAGS := 
+LDFLAGS :=
 ARFLAGS := rcs
 
-.PHONY : test tools
- 
+.PHONY : test tools cmc
+
 SRCEXT = c
 SOURCES := $(shell find $(SRCDIR) -type f -name *.$(SRCEXT))
 OBJECTS := $(patsubst $(SRCDIR)/%,$(BUILDDIR)/%,$(SOURCES:.$(SRCEXT)=.o))
 DEPS := $(OBJECTS:.o=.deps)
- 
+
 $(TARGET): $(OBJECTS)
-	@echo " Linking..."; $(AR) $(AR_OPTS) $(TARGET) $(OBJECTS) 
- 
+	@echo " Linking..."; $(AR) $(AR_OPTS) $(TARGET) $(OBJECTS)
+	@echo " Building CMC Libs..."; make -C ./cmc/
+
 $(BUILDDIR)/%.o: $(SRCDIR)/%.$(SRCEXT)
 	@mkdir -p $(BUILDDIR)
 	@echo " CC $<"; $(CC) $(CFLAGS) -MD -MF $(@:.o=.deps) -c -o $@ $<
- 
-docs: 
+
+docs:
 	@echo " Building Docs..."; $(DOXYGEN) ./doxygen/hmc_sim.cfg
 doclean:
 	@echo " Cleaning Docs..."; $(RM) -Rf ./doxygen/{html,latex,man,rtf,xml}
@@ -36,14 +37,18 @@ clean: doclean
 	@echo " Cleaning..."; $(RM) -r $(BUILDDIR) $(TARGET)
 test: $(TARGET)
 	@echo " Building Tests..."; make -C ./test/
+cmc:
+	@echo " Building CMC Libs..."; make -C ./cmc/
 tools:
 	@echo " Building Tools..."; make -C ./tools/
+cmcclean:
+	@echo " Cleaning CMC..."; make -C ./cmc/ clean
 toolsclean:
 	@echo " Cleaning Tools..."; make -C ./tools/ clean
 testclean:
 	@echo " Cleaning Tests..."; make -C ./test/ clean
-distclean: clean testclean doclean toolsclean
- 
+distclean: clean testclean doclean toolsclean cmcclean
+
 -include $(DEPS)
- 
+
 .PHONY: clean
---
+++ cmc/template/cmc.c
@@ -0,0 +1,68 @@
+/*
+ * _CMC_H_
+ *
+ * Implements a single CMC operation
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "hmc_sim.h"
+
+
+/* ----------------------------------------------------- GLOBALS */
+/* These globals define the CMC operation parameters, the request
+   enum and the command code.  Modify these values for your
+   respective CMC operation.  If more than one CMC library is used
+   in a simulation, these values MUST be unique across disparate
+   libraries.
+*/
+
+/* __op : Contains a bitwise OR'd list of operation parameters.
+        : See hmc_cmc_opt enums from hmc_sim_types.h
+*/
+static hmc_cmcop_t __op     = (INARG|OUTARG);
+
+/* __rqst : Contains the respective command enum that the simulated
+          : application uses to initiate a request for this command.
+          : See hmc_rqst_t enums from hmc_sim_types.h
+          : MUST BE UNIQUE ACROSS CMC LIBS
+*/
+static hmc_rqst_t __rqst    = CMC01;
+
+/* __cmd : Contains the respective command code for this CMC operation.
+         : This MUST match the __rqst field.  For example, if we have
+         : CMC32 as the __rqst, then the __cmd is (uint32_t)(32).
+*/
+static uint32_t __cmd       = 1;
+
+
+/* ----------------------------------------------------- HMCSIM_REGISTER_CMC */
+/*
+ * Registers the target CMC library instance with the core simulation. This
+ * function is loaded via dlopen and called from the HMC-Sim library when
+ * the sim makes a call to hmcsim_load_cmc().
+ *
+ * *op is a pointer to a valid hmc_cmcop_t for which the function returns
+ *     its CMC operation type.
+ *
+ * *rqst is a pointer to a valid hmc_rqst_t that defines which CMC operation
+ *     command enum that this library will utilize.  See the hmc_rqst_t
+ *     enums labeled CMCnn in ~/include/hmc_sim_types.h.
+ *
+ * *cmd is the respective command code that matches the *rqst command enum.
+ *     For example, if *rqst returns CMC32, then the *cmd is "32".
+ *
+ */
+extern int hmcsim_register_cmc( hmc_cmcop_t *op,
+                                hmc_rqst_t *rqst,
+                                uint32_t *cmd ){
+  *op   = __op;
+  *rqst = __rqst;
+  *cmd  = __cmd;
+
+  return 0;
+}
+
+/* EOF */
