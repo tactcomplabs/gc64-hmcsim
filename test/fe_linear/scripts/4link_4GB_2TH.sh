0dd67efd1d081dd30652e1bd3a429f91c631f60b
John Leidel <john.leidel@ttu.edu>
2016-03-01 21:24:59 -0600

    adding skeleton fe_linear test source

---
+++ test/fe_linear/scripts/4link_4GB_2TH.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ulimit -s unlimited
+
+MUTEX=../mutex
+RUNDIR=`pwd`
+
+BANKS=16
+CAPACITY=4
+LINKS=4
+BSIZE=64
+QDEPTH=64
+XDEPTH=128
+VAULTS=16
+DRAMS=20
+#THREADS=16
+#THREADS=32
+THREADS=2
+LIBSRC=../../../cmc/mutex/
+
+
+echo "Executing $MUTEX -L $LIBSRC -b $BANKS -c $CAPACITY -d $DRAMS -l $LINKS -m $BSIZE -n 1 -q $QDEPTH -v $VAULTS -x $XDEPTH -T $THREADS"
+
+
+$MUTEX -L $LIBSRC -b $BANKS -c $CAPACITY -d $DRAMS -l $LINKS -m $BSIZE -n 1 -q $QDEPTH -v $VAULTS -x $XDEPTH -T $THREADS >> $LINKS_$CAPACITY_$THREADS.txt 2>&1
