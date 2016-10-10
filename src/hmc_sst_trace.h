efbc2b6b0be2f55d28d28bf1874112762c3cd7d6
John Leidel <john.leidel@ttu.edu>
2016-10-10 09:19:20 -0500

    adding SST stats handlers; need more investigation for complex types

--- src/hmc_sst_trace.h
+++ src/hmc_sst_trace.h
@@ -15,6 +15,24 @@
 #include <stdlib.h>
 #include "hmc_sim.h"
 
+/* Global statistic values for SST Statistics tracing */
+/* -- stall values */
+Statistic<uint64_t>* XbarRqstStall;
+Statistic<uint64_t>* VaultRqstStall;
+Statistic<uint64_t>* XbarRspStall;
+Statistic<uint64_t>* RouteRqstStall;
+Statistic<uint64_t>* RouteRspStall;
+Statistic<uint64_t>* UndefStall;
+
+/* -- requests */
+
+/* -- bank conflicts */
+Statistic<uint64_t>* BankConflict;
+
+/* -- xbar latency */
+Statistic<uint64_t>* XbarLatency;
+
+
 #endif /* SST_ENABLE_STAT */
 #endif /* _HMC_SST_TRACE_H_ */
 
