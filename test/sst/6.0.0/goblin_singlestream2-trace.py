54d66840af8e076135d81012f9f17216dee30dd1
John Leidel <jleidel@tactcomplabs.com>
2016-10-20 10:37:36 -0500

    testing w/o tracing

--- test/sst/6.0.0/goblin_singlestream2-trace.py
+++ test/sst/6.0.0/goblin_singlestream2-trace.py
@@ -54,12 +54,12 @@ comp_memory.addParams({
       "backend.dram_count" : "20",
       "backend.capacity_per_device" : "4",
       "backend.xbar_depth" : "128",
-      "backend.max_req_size" : "128",
-      "backend.trace-banks" : "1",
-      "backend.trace-queue" : "1",
-      "backend.trace-cmds" : "1",
-      "backend.trace-latency" : "1",
-      "backend.trace-stalls" : "1"
+      "backend.max_req_size" : "128"
+      #"backend.trace-banks" : "1",
+      #"backend.trace-queue" : "1",
+      #"backend.trace-cmds" : "1",
+      #"backend.trace-latency" : "1",
+      #"backend.trace-stalls" : "1"
 })
 
 
