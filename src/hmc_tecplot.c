4f9ac9480c54d08ca7bd0a4eb6d828bb8fe7cab5
John Leidel <jleidel@tactcomplabs.com>
2017-02-07 17:10:36 -0600

    adding basic file handling for tecplot with prefixes

--- src/hmc_tecplot.c
+++ src/hmc_tecplot.c
@@ -10,22 +10,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 #include "hmc_sim.h"
 
-static int hmcsim_tecplot8( struct HMC8LinkTec Tec, uint64_t clock ){
+static int hmcsim_tecplot8( struct HMC8LinkTec Tec,
+                            uint64_t clock,
+                            char *prefix ){
+  /* vars */
+  char *fname_p = NULL;
+  char *fname_t = NULL;
+  FILE *ofile   = NULL;
+  /* ---- */
+
+  /* -- build the file names */
+  fname_p = malloc( sizeof( char ) * (strlen(prefix)+40) );
+  if( fname_p == NULL ){
+    return -1;
+  }
+  fname_t = malloc( sizeof( char ) * (strlen(prefix)+40) );
+  if( fname_t == NULL ){
+    return -1;
+  }
+
+  sprintf( fname_p, "%s%s%"PRIu64"%s", prefix, "-power.", clock, ".tec" );
+  sprintf( fname_t, "%s%s%"PRIu64"%s", prefix, "-therm.", clock, ".tec" );
+
+  /* -- write the power results */
+  /* -- open the file */
+  ofile = fopen( fname_p, "w" );
+
+  /* -- write all the data */
+
+  /* -- close the file */
+  fclose(ofile);
+  ofile = NULL;
+
+  /* -- write the thermal results */
+  /* -- open the file */
+  ofile = fopen( fname_t, "w" );
+
+  /* -- write all the data */
+
+  /* -- close the file */
+  fclose( ofile );
+  ofile = NULL;
+
   return 0;
 }
 
-static int hmcsim_tecplot4( struct HMC4LinkTec Tec, uint64_t clock ){
+static int hmcsim_tecplot4( struct HMC4LinkTec Tec,
+                            uint64_t clock,
+                            char *prefix ){
+  /* vars */
+  char *fname_p = NULL;
+  char *fname_t = NULL;
+  FILE *ofile   = NULL;
+  /* ---- */
+
+  /* -- build the file names */
+  fname_p = malloc( sizeof( char ) * (strlen(prefix)+40) );
+  if( fname_p == NULL ){
+    return -1;
+  }
+  fname_t = malloc( sizeof( char ) * (strlen(prefix)+40) );
+  if( fname_t == NULL ){
+    return -1;
+  }
+
+  sprintf( fname_p, "%s%s%"PRIu64"%s", prefix, "-power.", clock, ".tec" );
+  sprintf( fname_t, "%s%s%"PRIu64"%s", prefix, "-therm.", clock, ".tec" );
+
+  /* -- write the power results */
+  /* -- open the file */
+  ofile = fopen( fname_p, "w" );
+
+  /* -- write all the data */
+
+  /* -- close the file */
+  fclose(ofile);
+  ofile = NULL;
+
+  /* -- write the thermal results */
+  /* -- open the file */
+  ofile = fopen( fname_t, "w" );
+
+  /* -- write all the data */
+
+  /* -- close the file */
+  fclose( ofile );
+  ofile = NULL;
+
   return 0;
 }
 
 extern int hmcsim_tecplot( struct hmcsim_t *hmc ){
 
   if( hmc->num_links == 4 ){
-    return hmcsim_tecplot4( hmc->power.H4L, hmc->clk );
+    return hmcsim_tecplot4( hmc->power.H4L, hmc->clk, hmc->power.prefix );
   }else if( hmc->num_links == 8 ){
-    return hmcsim_tecplot8( hmc->power.H8L, hmc->clk );
+    return hmcsim_tecplot8( hmc->power.H8L, hmc->clk, hmc->power.prefix );
   }else{
     return -1;
   }
