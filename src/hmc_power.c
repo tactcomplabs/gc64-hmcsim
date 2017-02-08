c424d3991bf1cae1620dbba0bf7e6f440720b441
John Leidel <jleidel@tactcomplabs.com>
2017-02-08 16:33:11 -0600

    adding power tabulation for tecplot output for sub-device components

--- src/hmc_power.c
+++ src/hmc_power.c
@@ -91,6 +91,14 @@ extern int      hmcsim_power_vault_rsp_slot( struct hmcsim_t *hmc,
 
   hmcsim_trace_power_vault_rsp_slot( hmc, dev, quad, vault, slot );
 
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.vault_rsp_power[quad*vault] += hmc->power.vault_rsp_slot;
+    hmc->power.H4L.vault_rsp_btu[quad*vault] += (hmc->power.vault_rsp_slot*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.vault_rsp_power[quad*vault] += hmc->power.vault_rsp_slot;
+    hmc->power.H8L.vault_rsp_btu[quad*vault] += (hmc->power.vault_rsp_slot*HMC_MILLIWATT_TO_BTU);
+  }
+
   return 0;
 }
 
@@ -108,6 +116,14 @@ extern int      hmcsim_power_vault_rqst_slot( struct hmcsim_t *hmc,
 
   hmcsim_trace_power_vault_rqst_slot( hmc, dev, quad, vault, slot );
 
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.vault_rqst_power[quad*vault] += hmc->power.vault_rqst_slot;
+    hmc->power.H4L.vault_rqst_btu[quad*vault] += (hmc->power.vault_rqst_slot*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.vault_rqst_power[quad*vault] += hmc->power.vault_rqst_slot;
+    hmc->power.H8L.vault_rqst_btu[quad*vault] += (hmc->power.vault_rqst_slot*HMC_MILLIWATT_TO_BTU);
+  }
+
   return 0;
 }
 
@@ -119,9 +135,17 @@ extern int hmcsim_power_row_access( struct hmcsim_t *hmc,
     return -1;
   }
 
-  hmc->power.t_row_access += (hmc->power.row_access*(uint64_t)(mult));
+  hmc->power.t_row_access += (hmc->power.row_access*(float)(mult));
 
   hmcsim_trace_power_row_access( hmc, addr, mult );
+
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.row_access_power += (hmc->power.row_access*(float)(mult));
+    hmc->power.H4L.row_access_btu += ((hmc->power.row_access*(float)(mult))*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.row_access_power += (hmc->power.row_access*(float)(mult));
+    hmc->power.H8L.row_access_btu += ((hmc->power.row_access*(float)(mult))*HMC_MILLIWATT_TO_BTU);
+  }
 
   return 0;
 }
@@ -139,6 +163,14 @@ extern int hmcsim_power_xbar_rsp_slot( struct hmcsim_t *hmc,
 
   hmcsim_trace_power_xbar_rsp_slot( hmc, dev, link, slot );
 
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.xbar_rsp_power[link] += hmc->power.xbar_rsp_slot;
+    hmc->power.H4L.xbar_rsp_btu[link] += (hmc->power.xbar_rsp_slot*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.xbar_rsp_power[link] += hmc->power.xbar_rsp_slot;
+    hmc->power.H8L.xbar_rsp_btu[link] += (hmc->power.xbar_rsp_slot*HMC_MILLIWATT_TO_BTU);
+  }
+
   return 0;
 }
 
@@ -155,6 +187,14 @@ extern int hmcsim_power_xbar_rqst_slot( struct hmcsim_t *hmc,
 
   hmcsim_trace_power_xbar_rqst_slot( hmc, dev, link, slot );
 
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.xbar_rqst_power[link] += hmc->power.xbar_rqst_slot;
+    hmc->power.H4L.xbar_rqst_btu[link] += (hmc->power.xbar_rqst_slot*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.xbar_rqst_power[link] += hmc->power.xbar_rqst_slot;
+    hmc->power.H8L.xbar_rqst_btu[link] += (hmc->power.xbar_rqst_slot*HMC_MILLIWATT_TO_BTU);
+  }
+
   return 0;
 }
 
@@ -170,6 +210,14 @@ extern int hmcsim_power_vault_ctrl_transient( struct hmcsim_t *hmc,
 
   hmcsim_trace_power_vault_ctrl( hmc, vault );
 
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.vault_ctrl_power[vault] += p;
+    hmc->power.H4L.vault_ctrl_btu[vault] += (p*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.vault_ctrl_power[vault] += p;
+    hmc->power.H8L.vault_ctrl_btu[vault] += (p*HMC_MILLIWATT_TO_BTU);
+  }
+
   return 0;
 }
 
@@ -183,6 +231,14 @@ extern int hmcsim_power_vault_ctrl( struct hmcsim_t *hmc,
   hmc->power.t_vault_ctrl += hmc->power.vault_ctrl;
 
   hmcsim_trace_power_vault_ctrl( hmc, vault );
+
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.vault_ctrl_power[vault] += hmc->power.vault_ctrl;
+    hmc->power.H4L.vault_ctrl_btu[vault] += (hmc->power.vault_ctrl*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.vault_ctrl_power[vault] += hmc->power.vault_ctrl;
+    hmc->power.H8L.vault_ctrl_btu[vault] += (hmc->power.vault_ctrl*HMC_MILLIWATT_TO_BTU);
+  }
 
   return 0;
 }
@@ -204,6 +260,14 @@ extern int hmcsim_power_route_extern( struct hmcsim_t *hmc,
   hmcsim_trace_power_route_extern( hmc, srcdev, srclink, srcslot,
                                    destdev, destlink, destslot );
 
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.xbar_route_extern_power[srclink] += hmc->power.xbar_route_extern;
+    hmc->power.H4L.xbar_route_extern_btu[srclink] += (hmc->power.xbar_route_extern*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.xbar_route_extern_power[srclink] += hmc->power.xbar_route_extern;
+    hmc->power.H8L.xbar_route_extern_btu[srclink] += (hmc->power.xbar_route_extern*HMC_MILLIWATT_TO_BTU);
+  }
+
   return 0;
 }
 
@@ -221,6 +285,14 @@ extern int hmcsim_power_local_route( struct hmcsim_t *hmc,
   hmc->power.t_link_local_route += hmc->power.link_local_route;
 
   hmcsim_trace_power_local_route( hmc, dev, link, slot, quad, vault );
+
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.link_local_route_power[link] += hmc->power.link_local_route;
+    hmc->power.H4L.link_local_route_btu[link] += (hmc->power.link_local_route*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.link_local_route_power[link] += hmc->power.link_local_route;
+    hmc->power.H8L.link_local_route_btu[link] += (hmc->power.link_local_route*HMC_MILLIWATT_TO_BTU);
+  }
 
   return 0;
 }
@@ -240,11 +312,21 @@ extern int hmcsim_power_remote_route( struct hmcsim_t *hmc,
 
   hmcsim_trace_power_remote_route( hmc, dev, link, slot, quad, vault );
 
+  if( hmc->num_links == 4 ){
+    hmc->power.H4L.link_remote_route_power[link] += hmc->power.link_remote_route;
+    hmc->power.H4L.link_remote_route_btu[link] += (hmc->power.link_remote_route*HMC_MILLIWATT_TO_BTU);
+  }else{
+    hmc->power.H8L.link_remote_route_power[link] += hmc->power.link_remote_route;
+    hmc->power.H8L.link_remote_route_btu[link] += (hmc->power.link_remote_route*HMC_MILLIWATT_TO_BTU);
+  }
+
   return 0;
 }
 
 /* ----------------------------------------------------- HMCSIM_POWER_LINKS */
 extern int hmcsim_power_links( struct hmcsim_t *hmc ){
+
+  int i = 0;
 
   if( hmc == NULL ){
     return -1;
@@ -254,6 +336,18 @@ extern int hmcsim_power_links( struct hmcsim_t *hmc ){
                             * hmc->power.link_phy;
 
   hmcsim_trace_power_links( hmc );
+
+  if( hmc->num_links == 4 ){
+    for( i=0; i<4; i++ ){
+      hmc->power.H4L.link_phy_power[i] += hmc->power.link_phy;
+      hmc->power.H4L.link_phy_btu[i] += (hmc->power.link_phy*HMC_MILLIWATT_TO_BTU);
+    }
+  }else{
+    for( i=0; i<8; i++ ){
+      hmc->power.H8L.link_phy_power[i] += hmc->power.link_phy;
+      hmc->power.H8L.link_phy_btu[i] += (hmc->power.link_phy*HMC_MILLIWATT_TO_BTU);
+    }
+  }
 
   return 0;
 }
