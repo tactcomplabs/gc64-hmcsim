68fc9502d53e3ef4fb1470cc8008209290ff86e6
John Leidel <jleidel@tactcomplabs.com>
2017-02-28 09:11:35 -0600

    adding additional tests for stream power config and tecplot

---
+++ test/stream_power_config/scripts/runsimple.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "...executing 4link stream_power_config test"
+./simple4.sh
+echo "...executing 8link stream_power_config test"
+./simple8.sh
+
---
+++ test/stream_power_tecplot/scripts/runsimple.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "...executing stream_power_tecplot 4link test"
+./simple4.sh
+
+echo "...executing stream_power_tecplot 8link test"
+./simple8.sh
