eb35452145951d1d0626af821c032404941356df
John Leidel <jleidel@tactcomplabs.com>
2017-03-17 08:57:08 -0500

    fixing bug in sending posted packets via simple api and not clearning token state

--- src/hmc_send.c
+++ src/hmc_send.c
@@ -16,7 +16,121 @@
 
 /* ----------------------------------------------------- FUNCTION_PROTOTYPES */
 extern int hmcsim_util_zero_packet( struct hmc_queue_t *queue );
+extern uint32_t hmcsim_cmc_rawtoidx( uint32_t raw );
 
+
+/* ----------------------------------------------------- HMCSIM_POSTED_RSP */
+/*
+ * HMCSIM_POSTED_RSP
+ *
+ */
+static void hmcsim_posted_rsp( struct hmcsim_t *hmc, uint64_t hdr ){
+  uint32_t tag = 0;
+  uint32_t cmd = 0;
+
+  tag = (uint32_t)((hdr>>12)&0x7FF);
+  cmd = (uint32_t)(hdr&0x7F);
+
+  switch( cmd ){
+  case 24:
+  case 25:
+  case 26:
+  case 27:
+  case 28:
+  case 29:
+  case 30:
+  case 31:
+  case 95:
+  case 34:
+  case 35:
+  case 84:
+  case 33:
+    /* posted-type packet; no response */
+    hmc->tokens[tag].status = 2;
+    break;
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 20:
+  case 21:
+  case 22:
+  case 23:
+  case 32:
+  case 36:
+  case 37:
+  case 38:
+  case 39:
+  case 41:
+  case 42:
+  case 43:
+  case 44:
+  case 45:
+  case 46:
+  case 47:
+  case 56:
+  case 57:
+  case 58:
+  case 59:
+  case 60:
+  case 61:
+  case 62:
+  case 63:
+  case 69:
+  case 70:
+  case 71:
+  case 72:
+  case 73:
+  case 74:
+  case 75:
+  case 76:
+  case 77:
+  case 78:
+  case 85:
+  case 86:
+  case 87:
+  case 88:
+  case 89:
+  case 90:
+  case 91:
+  case 92:
+  case 93:
+  case 94:
+  case 102:
+  case 103:
+  case 107:
+  case 108:
+  case 109:
+  case 110:
+  case 111:
+  case 112:
+  case 113:
+  case 114:
+  case 115:
+  case 116:
+  case 117:
+  case 118:
+  case 120:
+  case 121:
+  case 122:
+  case 123:
+  case 124:
+  case 125:
+  case 126:
+  case 127:
+    /* cmc packet, check its response */
+    if( hmc->cmcs[hmcsim_cmc_rawtoidx(cmd)].rsp_cmd == RSP_NONE ){
+      /* cmc with posted response */
+      hmc->tokens[tag].status = 2;
+    }
+    break;
+  default:
+    /* normal response, just return */
+    return ;
+    break;
+  }
+}
 
 /* ----------------------------------------------------- HMCSIM_SEND */
 /*
@@ -161,6 +275,9 @@ extern int	hmcsim_send( struct hmcsim_t *hmc, uint64_t *packet )
 	for( i=0; i<t_len; i++ ) {
           queue->packet[i] = packet[i];
 	}
+
+        /* check for posted packets */
+        hmcsim_posted_rsp(hmc, packet[0]);
 
 #ifdef HMC_DEBUG
 	HMCSIM_PRINT_TRACE( "PACKET INJECTION SUCCESSFUL" );
