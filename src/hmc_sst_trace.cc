89cabfb69872b11baf952c5b6e576693cad5348b
John Leidel <jleidel@tactcomplabs.com>
2017-05-03 12:36:24 -0500

    hooking up SST Stats backend for all I/Os

--- src/hmc_sst_trace.cc
+++ src/hmc_sst_trace.cc
@@ -7,6 +7,7 @@
 
 #ifdef SST_ENABLE_STAT
 
+#include <string>
 #include "hmc_sst_trace.h"
 #include "hmc_sim.h"
 
@@ -158,9 +159,135 @@ int hmcsim_sst_trace_stall( struct hmcsim_t *hmc,
   return 0;
 }
 
+/* ----------------------------------------------------- HMCSIM_RQST_STR_TO_ENUM */
+static hmc_rqst_t hmcsim_rqst_str_to_enum( char *rqst ){
+  hmc_rqst_t R = FLOW_NULL;
+  std::string S(rqst);
+
+  if( S == "WR16" ){
+    R = WR16;
+  }else if( S == "WR32"){
+    R = WR32;
+  }else if( S == "WR48"){
+    R = WR48;
+  }else if( S == "WR64"){
+    R = WR64;
+  }else if( S == "WR80"){
+    R = WR80;
+  }else if( S == "WR96"){
+    R = WR96;
+  }else if( S == "WR112"){
+    R = WR112;
+  }else if( S == "WR128"){
+    R = WR128;
+  }else if( S == "WR256"){
+    R = WR256;
+  }else if( S == "RD16"){
+    R = RD16;
+  }else if( S == "RD32"){
+    R = RD32;
+  }else if( S == "RD48"){
+    R = RD48;
+  }else if( S == "RD64"){
+    R = RD64;
+  }else if( S == "RD80"){
+    R = RD80;
+  }else if( S == "RD96"){
+    R = RD96;
+  }else if( S == "RD112"){
+    R = RD112;
+  }else if( S == "RD128"){
+    R = RD128;
+  }else if( S == "RD256"){
+    R = RD256;
+  }else if( S == "MD_WR"){
+    R = MD_WR;
+  }else if( S == "MD_RD"){
+    R = MD_RD;
+  }else if( S == "BWR"){
+    R = BWR;
+  }else if( S == "TWOADD8"){
+    R = TWOADD8;
+  }else if( S == "ADD16"){
+    R = ADD16;
+  }else if( S == "P_WR16"){
+    R = P_WR16;
+  }else if( S == "P_WR32"){
+    R = P_WR32;
+  }else if( S == "P_WR48"){
+    R = P_WR48;
+  }else if( S == "P_WR64"){
+    R = P_WR64;
+  }else if( S == "P_WR80"){
+    R = P_WR80;
+  }else if( S == "P_WR96"){
+    R = P_WR96;
+  }else if( S == "P_WR112"){
+    R = P_WR112;
+  }else if( S == "P_WR128"){
+    R = P_WR128;
+  }else if( S == "P_WR256"){
+    R = P_WR256;
+  }else if( S == "P_BWR"){
+    R = P_BWR;
+  }else if( S == "P2ADD8"){
+    R = P_2ADD8;
+  }else if( S == "P2ADD16"){
+    R = P_2ADD16;
+  }else if( S == "FLOW_NULL"){
+    R = FLOW_NULL;
+  }else if( S == "PRET"){
+    R = PRET;
+  }else if( S == "TRET"){
+    R = TRET;
+  }else if( S == "IRTRY"){
+    R = IRTRY;
+  }else if( S == "2ADDS8R"){
+    R = TWOADDS8R;
+  }else if( S == "ADDS16R"){
+    R = ADDS16R;
+  }else if( S == "INC8"){
+    R = INC8;
+  }else if( S == "P_INC8"){
+    R = P_INC8;
+  }else if( S == "XOR16"){
+    R = XOR16;
+  }else if( S == "OR16"){
+    R = OR16;
+  }else if( S == "NOR16"){
+    R = NOR16;
+  }else if( S == "AND16"){
+    R = AND16;
+  }else if( S == "NAND16"){
+    R = NAND16;
+  }else if( S == "CASGT8"){
+    R = CASGT8;
+  }else if( S == "CASGT16"){
+    R = CASGT16;
+  }else if( S == "CASLT8"){
+    R = CASLT8;
+  }else if( S == "CASLT16"){
+    R = CASLT16;
+  }else if( S == "CASEQ8"){
+    R = CASEQ8;
+  }else if( S == "CASZERO16"){
+    R = CASZERO16;
+  }else if( S == "EQ8"){
+    R = EQ8;
+  }else if( S == "EQ16"){
+    R = EQ16;
+  }else if( S == "BWR8R"){
+    R = BWR8R;
+  }else if( S == "SWAP16"){
+    R = SWAP16;
+  }
+
+  return R;
+}
+
 /* ----------------------------------------------------- HMCSIM_SST_TRACE_RQST */
 extern "C" int hmcsim_sst_trace_rqst( struct hmcsim_t *hmc,
-                           hmc_rqst_t rqst,
+                           char *rqst,
                            uint32_t dev,
                            uint32_t quad,
                            uint32_t vault,
@@ -168,13 +295,16 @@ extern "C" int hmcsim_sst_trace_rqst( struct hmcsim_t *hmc,
                            uint64_t addr1,
                            uint32_t size );
 int hmcsim_sst_trace_rqst( struct hmcsim_t *hmc,
-                           hmc_rqst_t rqst,
+                           char *rqst,
                            uint32_t dev,
                            uint32_t quad,
                            uint32_t vault,
                            uint32_t bank,
                            uint64_t addr1,
                            uint32_t size ){
+
+  hmc_rqst_t rqst = hmcsim_rqst_str_to_enum( rqst );
+
   switch( rqst ){
   case WR16:
     Write16Ops->addData(1);
