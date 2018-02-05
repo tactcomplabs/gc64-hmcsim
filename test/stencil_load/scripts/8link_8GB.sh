8161d3f5eecfe96e008c3bde2b74a8f6f7674485
Dungeongate <teegan.duong@gmail.com>
2018-02-05 20:54:27 +0000

    added 1d and 3d stencil load CMC ops

---
+++ test/stencil_load/scripts/8link_8GB.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ulimit -s unlimited
+
+MUTEX=../mutex
+RUNDIR=`pwd`
+
+BANKS=16
+CAPACITY=8
+LINKS=8
+BSIZE=64
+QDEPTH=64
+XDEPTH=128
+VAULTS=64
+DRAMS=20
+#THREADS=16
+#THREADS=32
+THREADS=2
+LIBSRC=../../../cmc/mutex/
+
+
+while [ $THREADS -lt 101 ]
+do
+echo "Executing $MUTEX -L $LIBSRC -b $BANKS -c $CAPACITY -d $DRAMS -l $LINKS -m $BSIZE -n 1 -q $QDEPTH -v $VAULTS -x $XDEPTH -T $THREADS"
+$MUTEX -L $LIBSRC -b $BANKS -c $CAPACITY -d $DRAMS -l $LINKS -m $BSIZE -n 1 -q $QDEPTH -v $VAULTS -x $XDEPTH -T $THREADS >> $LINKS.$CAPACITY.$THREADS.txt 2>&1
+THREADS=$[$THREADS+1]
+done
---
+++ test/stencil_load/scripts/runall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./4link_4GB.sh
+./8link_8GB.sh
